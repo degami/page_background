3f042ec398ccea8b6f2426b703b5a2eceaf02eb5
Mirko De Grandis <degami@gmail.com>
2013-12-10 16:24:59 +0100

    nuovo file:   page_background_admin.js

---
+++ js/page_background_admin.js
@@ -0,0 +1,13 @@
+(function($){
+	$(document).ready(function(){
+		$('#edit-default').change(function(){
+			var $this = $(this);
+			$form = $this.closest('form');
+			if($this.attr('checked')){
+				$form.find('#edit-path').parent().hide();
+			}else{
+				$form.find('#edit-path').parent().show();
+			}
+		}).trigger('change');
+	});
+})(jQuery);
